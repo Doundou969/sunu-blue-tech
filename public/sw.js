3a864ef2ec92a65ec618c02324fa3de290d5107c
Doundou969 <kbayelayepro@gmail.com>
2026-01-28 04:45:58 +0000

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,18 +1,19 @@
-const CACHE_NAME = 'sunu-blue-v2';
-const ASSETS = [
-  'index.html',
-  'public/manifest.json',
-  'public/data.json'
+const CACHE_NAME = "pecheurconnect-v1";
+const urlsToCache = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "https://cdn.tailwindcss.com",
+  "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css",
+  "https://unpkg.com/leaflet@1.9.4/dist/leaflet.css",
+  "https://unpkg.com/leaflet@1.9.4/dist/leaflet.js",
+  "https://img.icons8.com/fluency/48/fish.png"
 ];
 
-self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
-  );
+self.addEventListener("install", e => {
+  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)));
 });
 
-self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    fetch(e.request).catch(() => caches.match(e.request))
-  );
+self.addEventListener("fetch", e => {
+  e.respondWith(caches.match(e.request).then(response => response || fetch(e.request)));
 });
