094ee924d0b8969a8182d0f85505de05199a1230
Doundou969 <kbayelayepro@gmail.com>
2026-01-28 17:29:17 +0000

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,22 +1,16 @@
-const CACHE_NAME = 'pecheurconnect-v1';
+const CACHE_NAME = "pecheurconnect-v1";
 const urlsToCache = [
-  '/sunu-blue-tech/index.html',
-  '/sunu-blue-tech/manifest.json',
-  '/sunu-blue-tech/icon-192.png',
-  '/sunu-blue-tech/icon-512.png',
-  '/sunu-blue-tech/data.json'
+  "/sunu-blue-tech/index.html",
+  "/sunu-blue-tech/data.json",
+  "/sunu-blue-tech/manifest.json",
+  "/sunu-blue-tech/icon-192.png",
+  "/sunu-blue-tech/icon-512.png"
 ];
 
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
-  );
+self.addEventListener("install", event => {
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)));
 });
 
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(resp => resp || fetch(event.request))
-  );
+self.addEventListener("fetch", event => {
+  event.respondWith(caches.match(event.request).then(resp => resp || fetch(event.request)));
 });
